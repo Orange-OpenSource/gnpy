a0bcc17c3b18b58955ef8156abca7d5813303c77
EstherLerouzic <esther.lerouzic@orange.com>
2022-04-01 18:36:15 +0200

    Fix: penalties are not correctly initialized
    When mode is not given and propagation is performed bidir, penalties
    corresponding to automatically  selected mode are not correctly
    initialized in request, and gnpy-path-request fails.
    
    Signed-off-by: EstherLerouzic <esther.lerouzic@orange.com>
    Change-Id: If045624ff5dad7f0dfdec93eaa05bb5eae86e643

--- gnpy/topology/request.py
+++ gnpy/topology/request.py
@@ -1144,6 +1144,7 @@ def compute_path_with_disjunction(network, equipment, pathreqlist, pathlist):
                         pathreq.OSNR = mode['OSNR']
                         pathreq.tx_osnr = mode['tx_osnr']
                         pathreq.bit_rate = mode['bit_rate']
+                        pathreq.penalties = mode['penalties']
                     # other blocking reason should not appear at this point
                 except AttributeError:
                     pathreq.baud_rate = mode['baud_rate']
@@ -1152,6 +1153,7 @@ def compute_path_with_disjunction(network, equipment, pathreqlist, pathlist):
                     pathreq.OSNR = mode['OSNR']
                     pathreq.tx_osnr = mode['tx_osnr']
                     pathreq.bit_rate = mode['bit_rate']
+                    pathreq.penalties = mode['penalties']
 
             # reversed path is needed for correct spectrum assignment
             reversed_path = find_reversed_path(pathlist[i])
