Telecominfraproject/oopt-gnpy

feat: add tx and rx ranges in library
Signed-off-by: JennyLescop <jenny.lescop@orange.com>
Change-Id: I87e4af979bcfadf1221354f538bd4ce3023ebac3

gnpy/topology/topology_parameters.py
@@ -137,7 +137,12 @@ class RequestParams(BaseParams):
         'power': None,
         'equalization_offset_db': None,
         'tx_power': None,
-        'tx_osnr': None
+        'tx_osnr': None,
+        'tx_channel_power_min': None,
+        'tx_channel_power_max': None,
+        'rx_channel_power_min': None,
+        'rx_channel_power_max': None,
+        'rx_ref_channel_power': None
     }
 
 

Orange-OpenSource/gnpy

gnpy/topology/topology_parameters.py
@@ -136,7 +136,12 @@ class RequestParams(BaseParams):
         'power': None,
         'equalization_offset_db': None,
         'tx_power': None,
-        'tx_osnr': None
+        'tx_osnr': None,
+        'blocking_reason': None,
+        'tx_channel_power_min_dbm': None,
+        'tx_channel_power_max_dbm': None,
+        'rx_channel_power_min_dbm': None,
+        'rx_channel_power_max_dbm': None
     }
 
 